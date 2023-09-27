714a98ed9b9b86425cfa2d10eeafbd353ced1470
Marcelo Buaretti <mbuaretti@gmail.com>
2023-09-27 09:29:06 -0300

    Create ex001.js

---
+++ exercicios/06/ex001.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`Sua velocidade é de ${vel}Km/h.`)
+if (vel > 80) {
+    console.log("Acima do limite de velocidade, MULTADO!")
+}
+
+console.log("Dirija sempre com o cinto de segurança.")
