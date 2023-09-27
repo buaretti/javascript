77a243541a2a3f8085f62a29ac20769c149816da
Marcelo Buaretti <mbuaretti@gmail.com>
2023-09-27 09:47:27 -0300

    Create ex002.js

---
+++ exercicios/06/ex002.js
@@ -0,0 +1,7 @@
+var pais = "EUA"
+console.log(`Vivendo em ${pais}`)
+if (pais == "Brasil"){
+    console.log("Nacionalidade: Brasileiro!")
+} else {
+    console.log("Nacionalidade: Estrangeiro")
+}
