d1a6526e74fd4ae290e91d0a0003ebf302f064c7
Marcelo Buaretti <mbuaretti@gmail.com>
2023-10-04 13:39:12 -0300

    .

--- exercicios/07/scripts/script.js
+++ exercicios/07/scripts/script.js
@@ -1,32 +1,57 @@
 function atualizarHora() {
     let date = new Date();
-    let horaatual = date.getHours();
+    let horaatual = date.getHours()
     let minutosatual = date.getMinutes();
     let segundosatual = date.getSeconds();
     let hora = document.getElementById("hora");
+    let calendario = document.getElementById("calendario")
+    let dia = date.getDate();
+    let mes = numeroParaMesExtenso(date.getMonth()); // Obtém o mês por extenso
+    let ano = date.getFullYear()
     let body = document.body;
     let imghora = document.getElementById("imghora");
 
-    hora.innerHTML = `Agora são ${horaatual} horas ${minutosatual} minutos e ${segundosatual} segundos.`;
+    hora.innerHTML = `Agora são ${horaatual} horas ${minutosatual} minutos e ${segundosatual} segundos ⏰`;
+
+    calendario.innerHTML = `Hoje é dia ${dia} de ${mes} de ${ano} 📆`;
 
     if (horaatual >= 0 && horaatual < 4) {
         // Madrugada
         imghora.src = "imgs/madrugada.jpg";
-        body.style.backgroundColor = '#000';
+        body.style.backgroundColor = '#CD8B72';
     } else if (horaatual >= 4 && horaatual < 12) {
         // Manhã
         imghora.src = "imgs/manha.jpg";
-        body.style.backgroundColor = '#D99E36';
-    } else if (horaatual >= 12 && horaatual < 18) {
+        body.style.backgroundColor = '#EDAD6B';
+    } else if (horaatual >= 12 && horaatual < 15) {
+        // Meio Dia
+        imghora.src = "imgs/meiodia.jpg";
+        body.style.backgroundColor = '#5E92D6';
+    } else if (horaatual >= 15 && horaatual < 18) {
         // Tarde
         imghora.src = "imgs/tarde.jpg";
-        body.style.backgroundColor = "#FEDC69";
-    } else if (horaatual >= 18 && horaatual < 24) {
+        body.style.backgroundColor = "#FAB04B";
+    } else if (horaatual >= 18 || horaatual < 4) {
         // Noite
         imghora.src = "imgs/noite.jpg";
         body.style.backgroundColor = "#41433A";
     }
+}
 
+// Função para converter número de mês para mês por extenso
+function numeroParaMesExtenso(numero) {
+    const meses = [
+        "Janeiro", "Fevereiro", "Março", "Abril",
+        "Maio", "Junho", "Julho", "Agosto",
+        "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+
+    // Verifique se o número está dentro do intervalo válido (0-11)
+    if (numero >= 0 && numero <= 11) {
+        return meses[numero];
+    } else {
+        return "Mês inválido";
+    }
 }
 
 // Atualize a cada segundo (1000 milissegundos)
