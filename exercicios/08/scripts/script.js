72168bf18d13923a75e7a1c429f3ce19cc6a3290
Marcelo Buaretti <mbuaretti@gmail.com>
2023-10-05 14:38:20 -0300

    .

--- exercicios/08/scripts/script.js
+++ exercicios/08/scripts/script.js
@@ -1,19 +1,76 @@
-var botao = document.getElementById("botao")
-botao.addEventListener("click", function(){
-    var anonasc = document.getElementById("anonasc").value
-    var mas = document.getElementById("mas").value
-    var fem = document.getElementById("fem").value
-    var res = document.getElementById("res")
-    var image = document.getElementById("image")
-    var date = new Date()
-    var anoatual = date.getFullYear()
-    var anos = Math.abs(anonasc - anoatual)
+var botao = document.getElementById("botao");
 
-    if(mas.value = checked){
-        var sexo = "Homem"
-    } else if(fem.value = checked){
-        var sexo = "Mulher"
+botao.addEventListener("click", function() {
+    var anonasc = document.getElementById("anonasc").value;
+    var res = document.getElementById("res");
+    var imgsex = document.getElementById("imgsex");
+    var date = new Date();
+    var anoatual = date.getFullYear();
+    var anos = Math.abs(anonasc - anoatual);
+    var radsex = document.querySelector('input[name="radsex"]:checked');
+    var sexo = "Indefinido"; // Valor padrão se nenhum botão for selecionado
+    var erro = document.getElementById("erro");
+    
+    if (radsex) {
+        sexo = radsex.id === "mas" ? "Homem" : "Mulher";
     }
     
-    res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`
-})
+    if (sexo === "Homem" && anos >= 0 && anos <= 4) {
+        // Mostrar a imagem e alterar o atributo src
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/bebe-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos >= 0 && anos <= 4) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/bebe-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Homem" && anos > 4 && anos <= 12) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/crianca-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos > 4 && anos <= 12) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/crianca-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Homem" && anos > 12 && anos <= 17) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adolescente-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos > 12 && anos <= 17) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adolescente-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Homem" && anos > 17 && anos <= 40) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adulto-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos > 17 && anos <= 40) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adulto-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Homem" && anos > 40 && anos <= 60) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adulto2-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos > 40 && anos <= 60) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/adulto2-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Homem" && anos > 60 && anos <= 100) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/idoso-m.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else if (sexo === "Mulher" && anos > 60 && anos <= 100) {
+        imgsex.style.display = "inline-block";
+        imgsex.src = "images/idoso-f.jpg";
+        res.innerHTML = `Detectamos ${sexo} com ${anos} anos.`;
+    } else {
+        // Se nenhuma condição for atendida, mostrar a mensagem de erro
+        erro.style.display = "block";
+        imgsex.style.display = "none";
+        res.innerHTML = ""; // Limpar o conteúdo anterior
+        setTimeout(function() {
+            erro.style.display = "none"; // Esconder a mensagem após 3 segundos
+        }, 3000); // 3000 milissegundos (3 segundos)
+    }
+});
