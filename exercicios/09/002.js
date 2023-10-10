22f5ab02338ce6a57218780b7f09170388d51f26
Marcelo Buaretti <mbuaretti@gmail.com>
2023-10-10 10:04:49 -0300

    Update 002.js

--- exercicios/09/002.js
+++ exercicios/09/002.js
@@ -1,5 +1,20 @@
-let numerosPares = [2,4,6,8,10,12,14,16,18,20]
+let numerosPares = [10,4,16,20,2,12,18,6,14,8]
+numerosPares.sort(function(a,b){
+    return a - b
+})
 
-for(var c = numerosPares.length; c != -1; c--){
+numerosPares.push(22)
+
+for(var c = 0; c != numerosPares.length; c++){
     console.log(numerosPares[c])
+}
+
+console.log(`Número total de elementos: ${numerosPares.length}`)
+console.log(`Primeiro elemento: ${numerosPares[0]}`)
+console.log(`Último elemento: ${numerosPares[10]}`)
+let pos = numerosPares.indexOf(2)
+if(pos == -1){
+    console.log("O valor não foi encontrado!")
+}else{
+    console.log(`A posição do número digitado é: ${pos}`)
 }
