29bff7a093c1e04d96429aeed959cf28c25622d9
Marcelo Buaretti <mbuaretti@gmail.com>
2023-12-27 11:56:41 -0300

    /

---
+++ curso js/docs/aula05.js
@@ -0,0 +1,20 @@
+/*> -> maior*/
+/*>= -> maior ou igual*/
+/*< -> menor*/
+/*<= -> menor ou igual*/
+/*== -> igual*/
+/*!= -> diferente*/
+
+let num = 3
+if(num != 5){
+    console.log("Diferente de 5!")
+} else {
+    console.log("Igual a 5!")
+}
+
+let num1 = 18
+if(num1 > 0 && num1 < 10){
+    console.log("Entre 0 e 10!")
+} else if(num1 > 10 && num1 < 20){
+    console.log("Entre 10 e 20")
+}
