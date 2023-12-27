243a0ce0026deec87362fc1fcfcd764ac0430ae4
Marcelo Buaretti <mbuaretti@gmail.com>
2023-12-27 11:42:40 -0300

    .

---
+++ curso js/docs/aula04.js
@@ -0,0 +1,23 @@
+let num1 = 0
+let num2 = 0
+let res = 0
+
+num1 = 12
+num2 = 5
+res = num1 + num2
+console.log(res)
+
+// exemplo de precedêndia nos operadores 
+
+let n1 = n2 = 0
+let media = 0
+n1 = 10
+n2 = 6
+media = (n1 + n2)/2
+console.log(media)
+
+/*num1 ++ -> adiciona 1 no valor de num1*/
+
+/*num1 -- -> retira 1 no valor de num1*/
+
+/*+=*/ 
