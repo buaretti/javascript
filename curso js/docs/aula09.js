f21a40a6b98e1915530214768aeaa34e77bc5c12
Marcelo Buaretti <mbuaretti@gmail.com>
2023-12-27 12:24:46 -0300

    Update aula09.js

---
+++ curso js/docs/aula09.js
@@ -0,0 +1,11 @@
+ let num = 10
+ let num1 = 10
+
+//  pré incremento - primeiro soma 1 ao valor e depois mostra o valor
+console.log(++num)
+
+// pós incremento - mostra o número primariamente e somente após isso soma 1 ao valor 
+
+console.log(num1++)
+
+// portanto, a posição do incremento importa
