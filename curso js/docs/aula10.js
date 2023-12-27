65ea72f3a0b5bc230d5851d487c5a04c54a7748b
Marcelo Buaretti <mbuaretti@gmail.com>
2023-12-27 13:01:17 -0300

    .

---
+++ curso js/docs/aula10.js
@@ -0,0 +1,16 @@
+let n = 1
+let res = (n % 2 == 0 ? "Par" : "Ímpar")
+console.log(`${n} é um número ${res}`)
+
+// modo mais extenso
+// if(n % 2 == 0){
+//     console.log(`${n} é um número PAR`)
+// } else {
+//     console.log(`${n} é um número ÍMPAR`)
+// }
+
+// Estrutura operador ternário -> Teste Lógico ? se sim : se não
+
+let num1 = 10, num2 = 4
+let res1 = (num1 > num2 ? "Maior" : "Menor")
+console.log(res1)
