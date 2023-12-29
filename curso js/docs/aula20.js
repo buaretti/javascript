a6d223db17df08bf182100658c0bce5dd14bcd80
Marcelo Buaretti <mbuaretti@gmail.com>
2023-12-29 12:57:11 -0300

    .

---
+++ curso js/docs/aula20.js
@@ -0,0 +1,50 @@
+/*function citar(){
+    console.log("cachorro")
+}
+* declaração da função */ 
+
+
+// for(let c = 0; c < 10; c++){
+//     citar()
+// *chamada da função* }
+
+// function media(x,y){
+//     return (x+y)/2
+// }
+// // função com return
+// console.log(media(10,5))
+
+// function soma(a,b){
+//     return a + b
+// }
+
+// console.log(`Resultado da soma: ${soma(100,59)}`)
+
+// function somar(){
+//     let n1 = 5
+//     let n2 = 10
+//     let res = n1 + n2
+//     return res
+// }
+
+// console.log(somar())
+
+// function nomear(){
+//     let res = "Marcelo"
+//     return res
+// }
+
+// let nome = nomear()
+// console.log(nome)
+
+function ParOuImpar(x){
+    if(x % 2 == 0){
+        return "PAR"
+    } else {
+        return "ÍMPAR"
+    }
+}
+
+let num = 19
+
+console.log(`O Número ${num} é: ${ParOuImpar(num)}`)
