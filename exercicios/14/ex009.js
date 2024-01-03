29bd35a4c532a8f8688039c375df4b4a049ee8a0
Marcelo Buaretti <mbuaretti@gmail.com>
2024-01-03 13:59:56 -0300

    Update ex009.js

--- exercicios/14/ex009.js
+++ exercicios/14/ex009.js
@@ -10,14 +10,14 @@ do {
     genero[c] = window.prompt("Qual o Sexo? [M/F]")
     idade[c] = window.prompt("Qual a idade?")
     cordocabelo[c] = window.prompt("Qual a cor do cabelo? [1] Preto [2] Castanho [3] Loiro [4] Ruivo")
-    if (genero[c] == "M" && idade[c] >= 18 && cordocabelo == 2) {
+    if (genero[c] === "M" && idade[c] >= 18 && cordocabelo[c] == 2) {
         homensfiltrados++
     }
-    if (genero[c] == "F" && idade[c] >= 25 && idade[c] <= 30 && cordocabelo[c] == 3) {
+    if (genero[c] === "F" && idade[c] >= 25 && idade[c] <= 30 && cordocabelo[c] == 3) {
         mulheresfiltradas++
     }
     c++
     resposta = window.prompt("Quer continuar? [S/N]")
-} while (resposta == "S" || resposta == 's')
+} while (resposta === "S" || resposta === 's')
 window.alert(`Total de homens com mais de 18 e cabelos castanhos: ${homensfiltrados}`)
 window.alert(`Total de mulheres entre 25 e 30 e cabelos loiros: ${mulheresfiltradas}`)
